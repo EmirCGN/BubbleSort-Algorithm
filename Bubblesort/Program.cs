4ec2a4fa1587c8fb30018edefebb7e63b730796a
Muhammed Emir Köroglu <muhammedem.k@gso.schule.koeln>
2023-06-07 12:42:35 +0200

    update

--- Bubblesort/Program.cs
+++ Bubblesort/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bubblesort
 {
@@ -22,38 +18,77 @@ namespace Bubblesort
                 array[i] = int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("Unsorted Array:");
+            Console.WriteLine();
+            Console.WriteLine("╔══════════════════════════╗");
+            Console.WriteLine("║      Unsorted Array      ║");
+            Console.WriteLine("╚══════════════════════════╝");
+            Console.WriteLine();
             PrintArray(array);
 
             BubbleSorter bubbleSorter = new BubbleSorter();
             bubbleSorter.Sort(array);
 
-            Console.WriteLine("Sorted Array:");
+            Console.WriteLine();
+            Console.WriteLine("╔═════════════════════════╗");
+            Console.WriteLine("║       Sorted Array      ║");
+            Console.WriteLine("╚═════════════════════════╝");
+            Console.WriteLine();
             PrintArray(array);
 
+            Console.WriteLine();
             Console.WriteLine("Enter a number to search in the sorted array:");
             int target = int.Parse(Console.ReadLine());
 
             BinarySearcher binarySearcher = new BinarySearcher();
             int index = binarySearcher.Search(array, target);
 
+            Console.WriteLine();
+            Console.WriteLine("╔══════════════════════════════════════════╗");
             if (index != -1)
             {
-                Console.WriteLine($"The number {target} was found at index {index}.");
+                Console.WriteLine($"║ The number {target} was found at index {index}! ║");
             }
             else
             {
-                Console.WriteLine($"The number {target} was not found in the array.");
+                Console.WriteLine($"║ The number {target} was not found in the array.  ║");
             }
+            Console.WriteLine("╚══════════════════════════════════════════╝");
         }
 
         static void PrintArray(int[] array)
         {
-            foreach (int element in array) //
+            Console.Write("╔");
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(element + " ");
+                Console.Write("════");
+                if (i < array.Length - 1)
+                {
+                    Console.Write("╦");
+                }
             }
-            Console.WriteLine();
+            Console.WriteLine("╗");
+
+            Console.Write("║");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]} ");
+                if (i < array.Length - 1)
+                {
+                    Console.Write("║");
+                }
+            }
+            Console.WriteLine("║");
+
+            Console.Write("╚");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("════");
+                if (i < array.Length - 1)
+                {
+                    Console.Write("╩");
+                }
+            }
+            Console.WriteLine("╝");
         }
     }
 }
